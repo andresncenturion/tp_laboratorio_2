0e88b135515583ebd95722ade95ca8df8f2e4ed3
Andres Centurion <andresncenturion@gmail.com>
2018-09-30 16:32:47 -0300

    TP 2

---
+++ TP 2/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+        {
+            
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                const short caloriasDulce = 80;
+                return caloriasDulce;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
