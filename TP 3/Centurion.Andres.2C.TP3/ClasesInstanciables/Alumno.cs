0280b168ef4fae7928a2358607ecfb71aa959fa9
alumno <alumno>
2018-11-08 20:26:15 -0300

    TP 3 hasta alumno

---
+++ TP 3/Centurion.Andres.2C.TP3/ClasesInstanciables/Alumno.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesAbstractas;
+
+namespace ClasesInstanciables
+{
+    public sealed class Alumno : Universitario
+    {
+        public enum EEstadoCuenta
+        {
+            AlDia, Deudor, Becado
+        }
+
+        private Universidad.EClases claseQueToma;
+        private EEstadoCuenta estadoCuenta;
+
+        public Alumno()
+        {
+
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma) : base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this.claseQueToma = claseQueToma;
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma, EEstadoCuenta estadoCuenta)  : this(id, nombre, apellido, dni, nacionalidad, claseQueToma)
+        {
+            this.estadoCuenta = estadoCuenta;
+        }
+
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine("Clase que toma: " + this.claseQueToma);
+            sb.AppendLine("Estado de cuenta: " + this.estadoCuenta);
+
+            return sb.ToString();
+        }
+
+        public static bool operator == (Alumno a, Universidad.EClases clase)
+        {
+            bool retorno = false;
+
+            if(a.claseQueToma == clase && a.estadoCuenta != EEstadoCuenta.Deudor)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator != (Alumno a, Universidad.EClases clase)
+        {
+            bool retorno = false;
+
+            if(a.claseQueToma != clase)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        protected override string ParticiparEnClase()
+        {
+            return ("TOMA CLASE DE " + this.claseQueToma);
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+    }
+}
---
+++ TP 3/Centurion.Andres.2C.TP3/ClasesInstanciables/Universidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesInstanciables
+{
+    public class Universidad
+    {
+        public enum EClases
+        {
+            Programacion, Laboratorio, Legislacion, SPD
+        }
+
+        
+    }
+}
