ccd2ece595449d33f16ce0e8e1ba241ae5026a5b
alumno <alumno>
2018-11-08 22:10:37 -0300

    TP3 profesor listo

---
+++ TP 3/Centurion.Andres.2C.TP3/ClasesInstanciables/Profesor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesAbstractas;
+
+namespace ClasesInstanciables
+{
+    public sealed class Profesor : Universitario
+    {
+        private Queue<Universidad.EClases> clasesDelDia;
+        private static Random random;
+
+        public Profesor()
+        {
+
+        }
+
+        static Profesor()
+        {
+            random = new Random();
+        }
+
+        public Profesor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad) : base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this.clasesDelDia = new Queue<Universidad.EClases>();
+            this.
+        }
+
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine("Clases del dia:" + this.clasesDelDia);
+
+            return sb.ToString();
+        }
+
+        private void _randomClases()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                this.clasesDelDia.Enqueue((Universidad.EClases)random.Next(0, 4));
+            }            
+        }
+
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Clases del dia: ");
+            if (!object.ReferenceEquals(this.clasesDelDia, null))
+            {
+                foreach (Universidad.EClases clase in this.clasesDelDia)
+                {
+                    sb.AppendLine("" + clase);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+
+    }
+}
